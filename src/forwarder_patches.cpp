5324684f6c185f164af9403b650d5daf0b616287
Andrew Hawes <117602754+Andrew1Hawes@users.noreply.github.com>
2024-07-24 21:19:38 +0100

    Forwarder display override: Allow falling back to DRC only

--- src/forwarder_patches.cpp
+++ src/forwarder_patches.cpp
@@ -4,12 +4,19 @@
 #include <sysapp/title.h>
 #include <wups/function_patching.h>
 
+static bool sAlreadyOverride = false;
+
 DECL_FUNCTION(int32_t, CMPTAcctSetScreenType, CmptScreenType type)
 {
+    /*  we need to allow falling back to DRC only,
+        so a "DRC only" argument is only overridden if this is the first time calling CMPTAcctSetScreenType(),
+        after that we never override a "DRC only" argument */
     if (gForwarderDisplayOverride != DISPLAY_OPTION_CHOOSE &&
-        OSGetTitleID() == _SYSGetSystemApplicationTitleId(SYSTEM_APP_ID_HEALTH_AND_SAFETY)) //only do for homebrew
-
+        (!sAlreadyOverride || type != CMPT_SCREEN_TYPE_DRC) &&
+        OSGetTitleID() == _SYSGetSystemApplicationTitleId(SYSTEM_APP_ID_HEALTH_AND_SAFETY)) { //only do for homebrew
+        sAlreadyOverride = true;
         type = (CmptScreenType) gForwarderDisplayOverride;
+    }
     return real_CMPTAcctSetScreenType(type);
 }
 
